abf250742451b0a5a68ee96980ebdf9f487b822f
numberbee7070 <yash.garg.cd.ece19@itbhu.ac.in>
2021-10-04 17:26:02 +0530

    footer added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/global_styles.css'
 import '../styles/theme-colors.css'
-
+import Footer from '../components/Footer'
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Component {...pageProps} />
+            <Footer />
+        </>
+    )
 }

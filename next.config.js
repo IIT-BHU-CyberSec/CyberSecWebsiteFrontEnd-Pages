d22b2c36259cc3a080cbe2442c2818dee6ece147
Eshwar S <75795114+EsharkyTheGreat@users.noreply.github.com>
2021-10-14 19:37:44 +0530

    Fixed typo

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
     env: {
         DEV_API_URL: 'http://localhost:5000',
         PRODUCTION_API_URL:
-            'https://api-iitbhucybersec-main.herokuapp.com/',
+            'https://api-iitbhucybersec-main.herokuapp.com',
     },
 }
